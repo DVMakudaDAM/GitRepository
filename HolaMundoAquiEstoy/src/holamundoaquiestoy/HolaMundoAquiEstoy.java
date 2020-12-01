32c9f24274553e8188560e8d484a1f7c598268d4
dam <dam@172.26.39.99>
2020-12-01 12:49:23 +0100

    Primera Version

---
+++ HolaMundoAquiEstoy/src/holamundoaquiestoy/HolaMundoAquiEstoy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoaquiestoy;
+
+/**
+ *
+ * @author dam
+ */
+public class HolaMundoAquiEstoy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Soy el principe de las espinas");
+    }
+    
+}
