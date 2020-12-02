23990ed71c8c645107f480f1a6606a91ccd27d64
dam <dam@172.26.39.99>
2020-12-02 13:53:34 +0100

    Primera version ejercicio Array

---
+++ Ejercicio_3_Array/src/ejercicio_3_array/Ejercicio_3_Array.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_3_array;
+
+/**
+ *
+ * @author dam
+ */
+public class Ejercicio_3_Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Concesionario negocio;
+        negocio = new Concesionario(3);
+    }
+    
+}
